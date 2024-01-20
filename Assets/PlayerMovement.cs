1f5d6b3c887bf0e263f12c4265d8ece3d45a6fa8
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2024-01-20 15:54:16 +0100

    Add movements(Running and Idle)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rbPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbPlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float dirX = Input.GetAxis("Horizontal");
+        rbPlayer.velocity = new Vector2(dirX * 7f, rbPlayer.velocity.y);
+        if (Input.GetKeyDown("space"))
+        {
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, 14f);
+        }
     }
 }
